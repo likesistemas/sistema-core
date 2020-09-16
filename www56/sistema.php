527e92532688e7fb9abd98e82243026b02661f49
Ricardo Paes <ricardo@likesistemas.com.br>
2020-09-16 15:29:55 -0300

    Issue #7 Correção gravação pastas temporárias sem sudo

--- www56/sistema.php
+++ www56/sistema.php
@@ -1,3 +1,30 @@
 <?php
 
+function testWriteFile($folder, $createFolder=false, $content='123') {
+    if( !file_exists($folder) && $createFolder ) {
+        mkdir($folder, 0777, true);
+    }
+
+    $src = $folder . "file.txt";
+    $myfile = fopen($src, "w") or die("Não conseguiu abrir o arquivo: '{$src}'.");
+    fwrite($myfile, $content);
+    fclose($myfile);
+
+    $writed = file_get_contents($src) or die("Não conseguiu ler o arquivo: '{$src}'.");
+    if($writed != $content) {
+        throw new Exception("Conteudo não é identico ao gravado. '{$writed}'");
+    }
+}
+
+testWriteFile("temp/");
+testWriteFile("temp/css/");
+testWriteFile("temp/js/");
+testWriteFile("temp/manifest/");
+testWriteFile("temp/object/");
+testWriteFile("temp/nfe/session/", true);
+testWriteFile("logs/");
+testWriteFile("logs/dia/", true);
+testWriteFile("files/");
+testWriteFile("files/produto/", true);
+
 echo "core";
